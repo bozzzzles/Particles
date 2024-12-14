0131f8b9f25f2f0417b5c2f9311b5e4db7bffcc4
thoAidan <ai.th.bus@gmail.com>
2024-12-14 14:36:17 -0800

    Fixed a goof in Engine.cpp

--- code/Engine.cpp
+++ code/Engine.cpp
@@ -8,3 +8,22 @@ Engine::Engine() {
 	// Create and open a window for the game
 	RenderWindow window(vm, "Particles", Style::Default);
 }
+
+void Engine::run() {
+    Clock localClock;
+    Time dt = localClock.getElapsedTime();
+    cout << "Starting Particle unit tests..." << endl;
+    Particle p(m_Window, 4, { (int)m_Window.getSize().x / 2, (int)m_Window.getSize().y / 2 });
+    p.unitTests();
+    cout << "Unit tests complete.  Starting engine..." << endl;
+}
+
+void Engine::input() {
+
+}
+void Engine::update(float dtAsSeconds) {
+
+}
+void Engine::draw() {
+
+}
