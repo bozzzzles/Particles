394931e8652d6159746880ea41fd182201ec63b9
thoAidan <ai.th.bus@gmail.com>
2024-12-14 13:40:35 -0800

    Defined Engine constructor

--- code/Engine.cpp
+++ code/Engine.cpp
@@ -1,1 +1,10 @@
 #include "Engine.h"
+
+Engine::Engine() {
+    int pixelWidth = VideoMode::getDesktopMode().width;
+	int pixelHeight = VideoMode::getDesktopMode().height;
+	VideoMode vm(pixelWidth, pixelHeight);
+
+	// Create and open a window for the game
+	RenderWindow window(vm, "Particles", Style::Default);
+}
