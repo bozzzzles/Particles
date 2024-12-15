f5b8b1201ef7947f9d6b1b3d4073c2877146fe11
Crosby Myers <crosby.myers4444@gmail.com>
2024-12-14 22:24:37 -0800

    added loop & edited to use member variables

--- code/Engine.cpp
+++ code/Engine.cpp
@@ -1,37 +1,40 @@
 #include "Engine.h"
 
-Engine::Engine() {
+Engine::Engine()
+{
     // Get display values to be used with window
-    int pixelWidth = VideoMode::getDesktopMode().width;
-	int pixelHeight = VideoMode::getDesktopMode().height;
-	VideoMode vm(pixelWidth, pixelHeight);
+    VideoMode vm(VideoMode::getDesktopMode().width, VideoMode::getDesktopMode().height);
 
-	// Create aCompleted run() from Engine.cppnd open a window for the game
-	RenderWindow window(vm, "Particles", Style::Default);
+    // Create a Completed run() from Engine.cpp and open a window for the game
+    m_Window.create(vm, "Particles", Style::Default);
 }
 
-void Engine::run() {
+void Engine::run()
+{
     // Construct a local clock to track time per frame
     Clock localClock;
-    Time dt = localClock.getElapsedTime();
+    Time dt;
     // Construct a local Particle to be used for the unit tests
     cout << "Starting Particle unit tests..." << endl;
-    Particle p(m_Window, 4, { (int)m_Window.getSize().x / 2, (int)m_Window.getSize().y / 2 });
+    Particle p(m_Window, 4, {(int)m_Window.getSize().x / 2, (int)m_Window.getSize().y / 2});
     p.unitTests();
     cout << "Unit tests complete.  Starting engine..." << endl;
     //  Create game loop
-    localClock.restart();
-    this->input();
-    this->update(dt.asSeconds());
-    this->draw();
+    while (m_Window.isOpen())
+    {
+        dt = localClock.restart();
+        input();
+        update(dt.asSeconds());
+        draw();
+    }
 }
 
-void Engine::input() {
-
+void Engine::input()
+{
 }
-void Engine::update(float dtAsSeconds) {
-
+void Engine::update(float dtAsSeconds)
+{
 }
-void Engine::draw() {
-
+void Engine::draw()
+{
 }
