8ef56bb8d1e158bfd96e08874ee8069920969c6f
thoAidan <ai.th.bus@gmail.com>
2024-12-15 12:29:30 -0800

    Completed menu header file

--- code/Menu.h
+++ code/Menu.h
@@ -1,7 +1,20 @@
 #pragma once
+#include "SFML/Graphics.hpp"
+
+using namespace sf;
+#define max_items 3
 
 class Menu {
   public:
-    Menu();
+    Menu(float width, float height);
     ~Menu();
+
+    void draw(RenderWindow &window);
+    void MoveUp();
+    void MoveDown();
+  private:
+    int selectedItemIndex;
+    Font font;
+    Text text[max_items];
+
 };
