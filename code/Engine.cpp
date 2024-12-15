9f4f8502f1fb817d8ab9b54b3c71d5b0eef12bec
Crosby Myers <crosby.myers4444@gmail.com>
2024-12-14 22:59:09 -0800

    Defined more Engine functions & added main

--- code/Engine.cpp
+++ code/Engine.cpp
@@ -4,7 +4,7 @@ Engine::Engine()
 {
     // Get display values to be used with window
     VideoMode vm(VideoMode::getDesktopMode().width, VideoMode::getDesktopMode().height);
-
+    srand(time(null));
     // Create a Completed run() from Engine.cpp and open a window for the game
     m_Window.create(vm, "Particles", Style::Default);
 }
@@ -31,10 +31,54 @@ void Engine::run()
 
 void Engine::input()
 {
+    Event event;
+    while (window.pollEvent(event))
+    {
+        if (event.type == Event::closed)
+        {
+            m_Window.close();
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Escape))
+        {
+            m_Window.close();
+        }
+        if (event.type == Event.MouseButtonPressed)
+        {
+            if (event.mouseButton.button == Mouse::Left)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Vector2i mousePixel(event.mouseButton.x, event.mouseButton.y);
+                    Particle p(m_Window, (rand() % 26) + 25, mousePixel);
+                    m_particles.push_back(p);
+                }
+            }
+        }
+    }
 }
+
 void Engine::update(float dtAsSeconds)
 {
+    for (auto it = m_particles.begin(); it != m_particles.end();)
+    {
+        if (it->getTTL() > 0.0)
+        {
+            it->update(dtAsSeconds);
+            it++;
+        }
+        else
+        {
+            it = m_particles.erase(it);
+        }
+    }
 }
+
 void Engine::draw()
 {
+    m_Window.clear();
+    for (Particle p : m_particles)
+    {
+        m_Window.draw(p);
+    }
+    m_Window.display();
 }
